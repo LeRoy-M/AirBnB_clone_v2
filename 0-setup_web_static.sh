183a5e5e4fcc9f010990be43c071c4b43bc80dee
LeRoy-M <lmwasela@gmail.com>
2023-12-26 17:52:29 +0300

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,4 +14,4 @@ printf %s "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx start
