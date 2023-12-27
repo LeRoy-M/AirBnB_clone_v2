d9aa37b556bf76b131d0a551e2aeff5e138e17bc
LeRoy-M <lmwasela@gmail.com>
2023-12-27 18:22:40 +0300

    5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,7 +1,12 @@
 # Manifest that sets up web servers for deployment of a static site
-exec {'setup nginx':
+exec {'setup web servers for deployment':
   command  => 'sudo apt-get -y update && sudo apt-get -y upgrade',
-'sudo apt-get -y install nginx',
-'sudo mkdir -p /data/web_static/releases/test /data/web_static/shared',
+              'sudo apt-get -y install nginx',
+              'sudo mkdir -p /data/web_static/releases/test /data/web_static/shared',
+              'printf %s "<h1>ALX School</h1>" > /data/web_static/releases/test/index.html',
+              'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+              'sudo chown -hR ubuntu:ubuntu /data/',
+              'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\' /etc/nginx/sites-available/default',
+              'sudo service nginx restart',
   provider => shell,
 }
