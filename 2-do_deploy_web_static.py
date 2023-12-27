1c5b93b76a2fafa1e73280400fd4a5946a4d6a9e
LeRoy-M <lmwasela@gmail.com>
2023-12-27 16:41:05 +0300

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,6 @@ archived package to the web servers, using `Fabric`
 """
 from fabric.api import env, put, run
 from os import path, symlink
-env.key_filename = "keyfile.pem"
 env.hosts = ["54.236.43.83", "54.164.92.21"]
 env.user = "ubuntu"
 
