342fe8dc2749ae3f4c62a14656a563d3ed94d29d
LeRoy-M <lmwasela@gmail.com>
2023-12-27 15:01:17 +0300

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ archived package to the web servers, using `Fabric`
 """
 from fabric.api import env, put, run
 from os import path, symlink
+env.user = "ubuntu"
 env.hosts = ["54.236.43.83", "54.164.92.21"]
 
 
@@ -14,8 +15,7 @@ def do_deploy(archive_path):
     try:
         filename_ext = archive_path.split("/")[-1]
         filename = filename_ext.split(".")[0]
-        path = "/data/web_static/releases/"
-        path_fn = f"{path}{filename}"
+        path_fn = f"/data/web_static/releases/{filename}"
         put(archive_path, "/tmp/{filename_ext}")
         run(f"mkdir -p {path_fn}/")
         run(f"tar -xvzf /tmp/{filename_ext} -C {path_fn}")
@@ -23,7 +23,6 @@ def do_deploy(archive_path):
         run(f"mv {path_fn}/web_static/* {path_fn}")
         run(f"rm -rf {path_fn}/web_static")
         run(f"rm -rf /data/web_static/current")
-        # run(f"ln -s {path_fn}/ /data/web_static/current")
         symlink(f"{path_fn}/", "/data/web_static/current")
         return True
     except Exception:
