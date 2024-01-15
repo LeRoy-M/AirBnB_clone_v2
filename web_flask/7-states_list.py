53bb49b8133e734326cd658800264c778a81bd89
LeRoy-M <lmwasela@gmail.com>
2024-01-15 15:45:53 +0300

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,10 +15,12 @@ def states_list():
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Handles closing of the storage"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
