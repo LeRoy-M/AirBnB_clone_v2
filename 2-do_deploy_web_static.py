3bd59411256e0e98a84728cb5c10c326fa5fa970
LeRoy-M <lmwasela@gmail.com>
2024-01-15 14:34:05 +0300

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,13 +11,14 @@ def do_deploy(archive_path):
     """Function that distributes the archive package"""
     if not path.exists(archive_path):
         return False
+
     try:
         filename_ext = archive_path.split("/")[-1]
         filename = filename_ext.split(".")[0]
         path_fn = f"/data/web_static/releases/{filename}"
         put(archive_path, "/tmp/{filename_ext}")
         run(f"mkdir -p {path_fn}/")
-        run(f"tar -xvzf /tmp/{filename_ext} -C {path_fn}")
+        run(f"tar -xvzf /tmp/{filename_ext} -C {path_fn}/")
         run(f"rm /tmp/{filename_ext}")
         run(f"mv {path_fn}/web_static/* {path_fn}")
         run(f"rm -rf {path_fn}/web_static")
